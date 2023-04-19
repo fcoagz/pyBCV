893e1d129496598056dde1aaaf2f140eec506906
fcoagz <grihardware@gmail.com>
2023-04-19 14:44:00 -0400

    creating new classes and making improvements

--- pyBCV/pyBCV.py
+++ pyBCV/pyBCV.py
@@ -1,97 +1,81 @@
+from typing import Any
+
 import requests
 from bs4 import BeautifulSoup
 
 requests.packages.urllib3.disable_warnings()
 
 class Currency:
-    def get_rate(self, currency_code=None):
-        webSite = 'https://www.bcv.org.ve/'
-        webResult = requests.get(webSite, verify=False)
+    def get_rate(self, currency_code=None) -> dict[str, str] | str:
+        response = requests.get('https://www.bcv.org.ve/', verify=False)
 
-        if webResult.status_code == 200: # OK!
-            dataWeb = BeautifulSoup(webResult.content, 'html.parser')
+        if response.status_code == requests.codes.ok:
+            soup = BeautifulSoup(response.content, 'html.parser')
 
-            label_html = dataWeb.find_all('div', 'col-sm-12 col-xs-12')
-            label_html_date = dataWeb.find('div', 'pull-right dinpro center')
-            priceResult = []
-            dateValid = []
+            rates_of_cambie = []
+            date_valid = []
 
-            for i in label_html:
-                x = i.find('strong')
-                priceResult.append(x.text.strip().replace(',', '.'))
-            
-            for z in label_html_date:
-                dateValid.append(z.text.strip())
+            for i in soup.find_all('div', 'col-sm-12 col-xs-12'):
+                rates_of_cambie.append(i.find('strong').text.strip().replace(',', '.'))
+            for x in soup.find('div', 'pull-right dinpro center'):
+                date_valid.append(x.text.strip())
 
             rates = {
-                'EUR' : f'Bs. {round(float(priceResult[-5]), 2)}',
-                'CNY' : f'Bs. {round(float(priceResult[-4]), 2)}',
-                'TRY' : f'Bs. {round(float(priceResult[-3]), 2)}',
-                'RUB' : f'Bs. {round(float(priceResult[-2]), 2)}',
-                'USD' : f'Bs. {round(float(priceResult[-1]), 2)}',
-                'DATE' : f'{dateValid[0]} {dateValid[1]}'
+                'EUR': f'Bs. {rates_of_cambie[-5]}',
+                'CNY': f'Bs. {rates_of_cambie[-4]}',
+                'TRY': f'Bs. {rates_of_cambie[-3]}',
+                'RUB': f'Bs. {rates_of_cambie[-2]}',
+                'USD': f'Bs. {rates_of_cambie[-1]}',
+                date_valid[0].replace(':', ''): date_valid[1].replace('  ', ' ')
             }
 
             if not currency_code:
                 return rates
-
+            
             elif currency_code in rates:
                 return rates[currency_code]
-            
     
-    def get_by_bank(self, bank_code=None, rate_or_sale=None):
-        webSite = 'https://www.bcv.org.ve/tasas-informativas-sistema-bancario'
-        webResult = requests.get(webSite, verify=False)
+class Bank:
+    def get_by_bank(self, bank_code=None, rate_or_sale=None) -> dict[Any, dict[str, str]] | str | dict[str, str]:
+        response = requests.get('https://www.bcv.org.ve/tasas-informativas-sistema-bancario', verify=False)
 
-        if webResult.status_code == 200: #OK!
-            dataWeb = BeautifulSoup(webResult.content, 'html.parser')
+        if response.status_code == requests.codes.ok:
+            soup = BeautifulSoup(response.content, 'html.parser')
 
-            label_html_date = dataWeb.find_all('td', 'views-field views-field-field-fecha-del-indicador')
-            label_html_bank = dataWeb.find_all('td', 'views-field views-field-views-conditional')
-            label_html_ratebuys = dataWeb.find_all('td', 'views-field views-field-field-tasa-compra')
-            label_html_ratesales = dataWeb.find_all('td', 'views-field views-field-field-tasa-venta')
+            date_indicator = []
+            title_bank = []
+            rate_buys = []
+            rate_sales = []
 
-            dateIndicator = []
-            titleBank = []
-            rateBuys = []
-            rateSales = []
-
-            for w in label_html_date:
-                dateIndicator.append(w.text.strip())
-            for x in label_html_bank:
-                titleBank.append(x.text.strip())
-            for y in label_html_ratebuys:
-                rateBuys.append(y.text.strip().replace(',', '.'))
-            for z in label_html_ratesales:
-                rateSales.append(z.text.strip().replace(',', '.'))
-
-            titleBank[3] = titleBank[3].replace('é', 'e')
-
+            for i in soup.find_all('td', 'views-field views-field-field-fecha-del-indicador'):
+                date_indicator.append(i.text.strip())
+            for j in soup.find_all('td', 'views-field views-field-views-conditional'):
+                title_bank.append(j.text.strip())
+            for k in soup.find_all('td', 'views-field views-field-field-tasa-compra'):
+                rate_buys.append(k.text.strip().replace(',', '.'))
+            for e in soup.find_all('td', 'views-field views-field-field-tasa-venta'):
+                rate_sales.append(e.text.strip().replace(',', '.'))
+            
             bank = {
-                titleBank[0]: {
-                'Fecha' : f'{dateIndicator[0]}',
-                'Compra' : f'Bs. {round(float(rateBuys[0]), 2)}',
-                'Venta' : f'Bs. {round(float(rateSales[0]), 2)}'
+                title_bank[0]: {
+                'Fecha': date_indicator[0],
+                'Compra': f'Bs. {rate_buys[0]}',
+                'Venta': f'Bs. {rate_sales[0]}'
                 },
-                titleBank[1]: {
-                'Fecha' : f'{dateIndicator[1]}',
-                'Compra' : f'Bs. {round(float(rateBuys[1]), 2)}',
-                'Venta' : f'Bs. {round(float(rateSales[1]), 2)}'
+                title_bank[1]: {
+                'Fecha': date_indicator[1],
+                'Compra': f'Bs. {rate_buys[1]}',
+                'Venta': f'Bs. {rate_sales[1]}'
                 },
-                titleBank[2]: {
-                'Fecha' : f'{dateIndicator[2]}',
-                'Compra' : f'Bs. {round(float(rateBuys[2]), 2)}',
-                'Venta' : f'Bs. {round(float(rateSales[2]), 2)}'
+                title_bank[2]: {
+                'Fecha': date_indicator[2],
+                'Compra': f'Bs. {rate_buys[2]}',
+                'Venta': f'Bs. {rate_sales[2]}'
                 },
-                titleBank[3]: {
-                'Fecha' : f'{dateIndicator[3]}',
-                'Compra' : f'Bs. {round(float(rateBuys[3]), 2)}',
-                'Venta' : f'Bs. {round(float(rateSales[3]), 2)}'
-                },
-                titleBank[4]: {
-                'Fecha' : f'{dateIndicator[4]}',
-                'Compra' : f'Bs. {round(float(rateBuys[4]), 2)}',
-                'Venta' : f'Bs. {round(float(rateSales[4]), 2)}'
+                title_bank[3]: {
+                'Fecha': date_indicator[2],
+                'Compra': f'Bs. {rate_buys[2]}',
+                'Venta': f'Bs. {rate_sales[3]}'
                 },
             }
             
