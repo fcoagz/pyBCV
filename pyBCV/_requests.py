e39927b7f47cb7677177f7bd841fb1c3dbf1912e
Francisco Griman <103836660+fcoagz@users.noreply.github.com>
2024-01-01 04:58:40 -0400

    updating _requests.py

--- pyBCV/_requests.py
+++ pyBCV/_requests.py
@@ -7,10 +7,3 @@ def _ensure_200_response(url: str) -> requests.Response:
     response.raise_for_status()
 
     return response
-
-p = _ensure_200_response('https://www.bcv.org.ve/cambiaria/export/tasas-informativas-sistema-bancario')
-from bs4 import BeautifulSoup
-
-soup = BeautifulSoup(p.content, "html.parser")
-for x in soup.find_all('tr'):
-    print(x.find_all('td'))
